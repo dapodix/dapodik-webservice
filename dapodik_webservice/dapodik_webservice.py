a83eff894b55b59bb6e7e4ca08554dc2f1e57a0c
hexatester <revolusi147id@gmail.com>
2020-12-29 18:17:48 +0700

    add docstrings

--- dapodik_webservice/dapodik_webservice.py
+++ dapodik_webservice/dapodik_webservice.py
@@ -1,5 +1,5 @@
 from requests import get as requests_get
-from typing import Any, List
+from typing import Any, AnyStr, List, Union
 
 from dapodik_webservice.models import (
     Pengguna,
@@ -13,40 +13,74 @@ from dapodik_webservice.util import handle_response
 
 
 class DapodikWebservice(object):
+    """DapodikWebservice
+
+    Args:
+        token (Union[AnyStr, int]): Token Web Service Dapodik
+        npsn (AnyStr): Nomor NPSN Sekolah
+        url (str, optional): Url aplikasi dapodik (tanpa / di belakang). Defaults to "http://localhost:5774".
+        user_agent (str, optional): User-Agent yang digunakan. Defaults to "pypi.org/project/dapodik_webservice/".
+    """
+
     def __init__(
         self,
-        token: str,
-        npsn: str,
+        token: Union[AnyStr, int],
+        npsn: AnyStr,
         url: str = "http://localhost:5774",
         user_agent: str = "pypi.org/project/dapodik_webservice/",
     ):
-        self.token = token
-        self.npsn = npsn
+        self.token = str(token)
+        self.npsn = str(npsn)
         self.url = url
         self._ua = user_agent
 
     @property
     def pengguna(self) -> List[Pengguna]:
+        """Mendapatkan daftar pengguna Dapodik
+
+        Returns:
+            List[Pengguna]: list Pengguna
+        """
         res = self._get("getSekolah")
         return Pengguna.from_result(res)
 
     @property
     def peserta_didik(self) -> List[PesertaDidik]:
+        """Mendapatkan daftar peserta didik Dapodik
+
+        Returns:
+            List[PesertaDidik]: list PesertaDidik
+        """
         res = self._get("getPesertaDidik")
         return PesertaDidik.from_result(res)
 
     @property
     def ptk(self) -> List[Ptk]:
+        """Mendapatkan daftar ptk / gtk Dapodik
+
+        Returns:
+            List[Ptk]: list Ptk
+        """
         res = self._get("getGtk")
         return Ptk.from_result(res)
 
     @property
     def rombongan_belajar(self) -> List[RombonganBelajar]:
+        """Mendapatkan daftar rombongan belajar Dapodik
+
+        Returns:
+            List[RombonganBelajar]: list RombonganBelajar
+        """
         res = self._get("getRombonganBelajar")
         return RombonganBelajar.from_result(res)
 
     @property
     def sekolah(self) -> Sekolah:
+        """Mendapatkan data sekolah Dapodik
+
+        Returns:
+            Sekolah: list RombonganBelajar
+        """
         res = self._get("getSekolah")
         return Sekolah.from_result(res)
 
