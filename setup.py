5a99a82a3996a6d152df83bfebb78f8c95b32bad
hexatester <revolusi147id@gmail.com>
2021-01-05 01:58:17 +0700

    remove dacite

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     keywords="dapodik dapodik-webservice kemdikbud",
     description="SDK Python Web Service aplikasi Dapodik",
     packages=packages,
-    install_requires=["python", "requests", "dacite", "attrs"],
+    install_requires=["python", "requests", "attrs"],
     include_package_data=True,
     classifiers=[
         "Development Status :: 4 - Beta",
